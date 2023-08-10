46165189140d98c745c54b65ab462e73de4f3da8
rajanyabasak <141635686+rajanyabasak@users.noreply.github.com>
2023-08-10 17:17:31 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
